e99db405152bf56ca215b439973d8ec74dd73727
wlsn <gwillson.camp@gmail.com>
2020-02-24 23:52:59 -0500

    Formatting.

--- lin_audit.sh
+++ lin_audit.sh
@@ -10,7 +10,8 @@
 #       OPTIONS:  ---
 #  REQUIREMENTS:  ---
 #          BUGS:  ---
-#         NOTES:  Please do not modify this file without checking with Willson Camp (contact below).  Modifcation of variables
+#         NOTES:  Please do not modify this file without checking with
+#						Willson Camp (contact below).  Modifcation of variables
 #	          could cause the script to remove files that it shouldn't.
 #        AUTHOR:  Willson Camp
 #
